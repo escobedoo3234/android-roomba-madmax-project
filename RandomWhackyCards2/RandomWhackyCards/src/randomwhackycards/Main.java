124a83e3bfb3eee2f5b2433bf59e5b18e1d40a1b
william <william@10.0.1.6>
2012-02-09 17:09:35 -0800

    working on game

--- RandomWhackyCards2/RandomWhackyCards/src/randomwhackycards/Main.java
+++ RandomWhackyCards2/RandomWhackyCards/src/randomwhackycards/Main.java
@@ -1,19 +1,23 @@
 package randomwhackycards;
 
-/*******************************
- * 12/6/11 commit by Vic
- *******************************/
+/**
+ * *****************************
+ * 12/6/11 commit by Vic *****************************
+ */
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
 import java.util.Random;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
-public class Main implements Runnable
+public class Main implements Runnable, ActionListener, MouseListener
 {
-
+    
     private int width = Toolkit.getDefaultToolkit().getScreenSize().width;
     private int height = Toolkit.getDefaultToolkit().getScreenSize().height;
     public JFrame cardWindow;
@@ -21,40 +25,57 @@ public class Main implements Runnable
     private int cardSize = 4;
     private int jillScore;
     private int jackScore;
-    public WhackyButton jack;
+//    public WhackyButton jack;
     public int winningScore = 400;
     public int[][] intArray;
     public JButton[][] buttonArray;
     public Random r;
-
+    private JButton j;
+    private Icon marioPic;
+    private URL iconAddress;
+    private Timer timer;
+    private int presentNumber;
+    
     public static void main(String[] args)
     {
         SwingUtilities.invokeLater(new Main());
     }
-
+    
     @Override
     public void run()
     {
-        buttonArray = new JButton[4][4];
-        intArray = new int[4][4];
+        buttonArray = new JButton[cardSize][cardSize];
+        intArray = new int[cardSize][cardSize];
+        iconAddress = getClass().getResource("MarioBrosArtwork.jpg");
+        marioPic = new ImageIcon(iconAddress);
+        timer = new Timer(2000, this);
+        timer.setRepeats(false);
         r = new Random();
         cardWindow = new JFrame("RandomWhackyCards");//make jframe
         cardWindow.setSize(width, height);//set size
         cardWindow.setVisible(true);//set visible
         cardWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//make it able to close
         cardWindow.setLayout(new GridLayout(cardSize, cardSize));
-        jack = new WhackyButton(this);
-        jack.setR(r);
-        jack.setIntArray(intArray);
-        for (int i = 0; i < 4; i++)
+        
+        for (int row = 0; row < cardSize; row++)
         {
-            for (int j = 0; j < 4; j++)
+            for (int col = 0; col < cardSize; col++)
             {
-                intArray[i][j] = r.nextInt(100); 
+                j = new JButton(marioPic);
+                j.setMnemonic(r.nextInt(100));
+                cardWindow.add(j);
+                j.addMouseListener(this);
+                j.setFont(new Font("Bank Gothic", Font.BOLD, 90));
+                int tempr = r.nextInt(3);
+                if (row == tempr)
+                {
+                    intArray[row][tempr] = r.nextInt(5);
+                    j.setMnemonic((int)(Math.random() * 100) + 100);
+                }
             }
         }
     }
-
+    
     public void updateScore(int n)
     {
         clicks++;
@@ -88,5 +109,50 @@ public class Main implements Runnable
             JOptionPane.showMessageDialog(null, "Draw no one won. Jill was winning by " + (jillScore - jackScore));
             System.exit(0);
         }
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        timer.stop();
+        presentNumber = 0;
+        j.setText("");
+    }
+    
+    @Override
+    public void mouseClicked(MouseEvent me1)
+    {
+    }
+    
+    @Override
+    public void mousePressed(MouseEvent me2)
+    {
+        JButton temp = (JButton) me2.getSource();
+        try
+        {
+            presentNumber = Integer.parseInt(temp.getText());
+        } catch (NumberFormatException e2)//no cheating
+        {
+            temp.setIcon(null);
+            temp.setFont(new Font("Ariel", Font.BOLD, 99));
+            temp.setText("" + temp.getMnemonic());
+            updateScore(temp.getMnemonic());
+            timer.start();
+        }
+    }
+    
+    @Override
+    public void mouseReleased(MouseEvent me3)
+    {
+    }
+    
+    @Override
+    public void mouseEntered(MouseEvent me4)
+    {
+    }
+    
+    @Override
+    public void mouseExited(MouseEvent me5)
+    {
     }
 }
