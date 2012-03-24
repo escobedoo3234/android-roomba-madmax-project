fc446da4d5470ecb1e2043682bee189deac9ec69
william <william@10.0.1.6>
2012-03-23 19:17:44 -0700

    roomba bump methods work and we can make a square.

---
+++ iRobot2012/src/org/wintrisstech/erik/iaroc/MadMaxSkatingMonkeys.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.wintrisstech.erik.iaroc;
+
+/**
+ *
+ * @author william
+ */
+public class MadMaxSkatingMonkeys
+{
+//    first attempt a square
+//             north
+//            turnAndGo(0, 500);
+//            turnAndGo(90, 500);
+//            // east
+//            turnAndGo(90, 500);
+//            // south
+//            turnAndGo(90, 500);
+//            // west
+//            driveDirect(0, 0);// stop
+//            Random angle = new Random();
+//            while (true)
+//            {
+//                readSensors(SENSORS_GROUP_ID6);
+//                if (isBumpRight())
+//                {
+//                    turnAndGo(45, 200);
+//                }
+//                if (isBumpLeft())
+//                {
+//                    turnAndGo(-45, 200);
+//                }
+//                if (isBumpLeft() && isBumpRight())
+//                {
+//                    turnAndGo(0, -200);
+//                    int r = angle.nextInt(180);
+//                    turnAndGo(r - 90, 200);
+//                }
+//                driveDirect(100, 100);
+//            }
+////            song(1, a);
+////            playSong(1);
+}
